d2b202df28ae9935dd71e4fac0f47323d56b709e
APCS-CodeHS <codeapcs@gmail.com>
2017-04-18 14:05:29 -0400

    Update 3.2.6- Add10.java

--- Methods/3.2.6- Add10.java
+++ Methods/3.2.6- Add10.java
@@ -1,24 +1,18 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf820
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
 
-\f0\fs24 \cf0 public class Add10 extends ConsoleProgram\
-\{\
-    public void run()\
-    \{\
-        add10(1);\
-        add10(100);\
-        add10(-50);\
-    \}\
-    \
-    private void add10(int a)\
-    \{\
-        int total = a + 10;\
-        System.out.println(total);\
-    \}\
-\}\
-\
+public class Add10 extends ConsoleProgram
+{
+    public void run()
+    {
+        add10(1);
+        add10(100);
+        add10(-50);
+    }
+    
+    private void add10(int a)
+    {
+        int total = a + 10;
+        System.out.println(total);
+    }
+}
+
 }
