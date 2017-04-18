3d21a272859bfffdae7d0757deb9e5fee158085e
APCS-CodeHS <codeapcs@gmail.com>
2017-04-18 14:16:57 -0400

    Create 3.5.7: First and Last.java

---
+++ Methods/3.5.7: First and Last.java
@@ -0,0 +1,7 @@
+public String firstAndLast(String str)
+{
+      char chf = str.charAt(0);
+	  char chl = str.charAt(str.length()-1);
+	  String firstAndLast = "" + chf + chl;
+	  return firstAndLast;
+}
