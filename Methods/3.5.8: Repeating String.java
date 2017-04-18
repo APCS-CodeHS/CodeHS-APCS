522e18a0fde4ad80d944b82e48f6849fb752b424
APCS-CodeHS <codeapcs@gmail.com>
2017-04-18 14:17:33 -0400

    Create 3.5.8: Repeating String.java

---
+++ Methods/3.5.8: Repeating String.java
@@ -0,0 +1,12 @@
+public String repeatString(String str, int n)
+{
+  String text = "";
+  
+  for (int i = 0; i < n; i++) 
+      {
+        text = text + str;
+      }
+      
+    return text;
+}
+
